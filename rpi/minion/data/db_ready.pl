d38c6164d5e6117b6ee8fe6797f4c9ca6dd44f89
Kiel <kielstr@cpan.org>
2017-12-04 11:59:54 +1100

    Rpi hacking

--- rpi/minion/data/db_ready.pl
+++ rpi/minion/data/db_ready.pl
@@ -6,24 +6,17 @@ use feature 'say';
 use Env qw( POSTGRES_DB POSTGRES_USER POSTGRES_PASSWORD POSTGRES_HOST );
 use DBI;
 
-my ( $dbh_mysql, $dbh_pg );
+my ( $dbh_mysql );
 my $dsn = "DBI:mysql:database=network_management;host=mysql;port=3306";
 
-until ( $dbh_pg and $dbh_mysql ) {
-
-	$dbh_pg = DBI->connect( 
-		"dbi:Pg:dbname=mic;host=postgres", 
-		'mic',
-		'mic'
-	) ;
+until ( $dbh_mysql ) {
 
 	$dbh_mysql = DBI->connect($dsn, 'nm', 'Tripper');
 
 	sleep 1;
-	say 'waiting for databases to allow connections';
+	say 'waiting for database to allow connections';
 }
 
-$dbh_pg->disconnect;
 $dbh_mysql->disconnect;
 
 exec @ARGV;
